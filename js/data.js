45fbe6035f5bba70d2848c5e4e716bc5fe59edf6
Guido <ppapaguido@gmail.com>
2021-11-23 16:12:56 +0100

    problemi addeventlistener

--- js/data.js
+++ js/data.js
@@ -1,4 +1,5 @@
-[
+
+const divs = [
 	{
 		name: 'cat',
 		prefix: 'fa-',
@@ -112,3 +113,62 @@
 		color: 'blue'
 	}
 ];
+const container = document.querySelector('.container');
+
+for (let i = 0; i < divs.length; i++) {
+	container.innerHTML += `
+	<div class="bg-light px-5  rounded-3">
+	<div class="fs-3 text-center"><i class="${divs[i].family} ${divs[i].prefix}${divs[i].name}"></i></div>
+	<div class="text-uppercase text-center">${divs[i].name}</div>
+	`
+}
+
+for (let i = 0; i < divs.length; i++){
+	const classe = document.getElementsByClassName('fas');
+	if (divs[i].color == 'blue') {
+		classe[i].classList.add('color-blue')
+	} else if (divs[i].color == 'orange'){
+		classe[i].classList.add('color-orange')
+	} else {
+		classe[i].classList.add('color-green')
+
+	}	
+}
+
+const selezione = document.getElementById('tipo-font');
+
+selezione.addEventListener('change', function(){
+	console.log(selezione.value);
+	if (selezione.value == 'all'){
+	}
+});
+
+
+
+const colori = divs.color;
+console.log(colori);
+
+divs.forEach( div => {
+	console.log(`${div.color}`, 'ciao');
+});
+
+divs.forEach( div => {
+	if (div.color == 'orange'){
+		console.log('questo e arancione');
+	} else if (div.color == 'blue'){
+		console.log('questo e blue');
+	} else{
+		console.log('questo e verde');
+	}
+})
+
+
+function creazioneDiv () {
+	for (let i = 0; i < divs.length; i++) {
+		container.innerHTML += `
+		<div class="bg-light px-5  rounded-3">
+		<div class="fs-3 text-center"><i class="${divs[i].family} ${divs[i].prefix}${divs[i].name}"></i></div>
+		<div class="text-uppercase text-center">${divs[i].name}</div>
+		`
+	}
+}
